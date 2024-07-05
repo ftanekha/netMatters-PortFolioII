807407dd47c4a196fa6ff10104cd4c721ab8a9c5
ftanekha <farai.tanekha@gmail.com>
2024-07-05 17:12:40 +0100

    JS ISSUE: error in console when form validation function executes SOL: only execute function when URL matches the page containing the form
    -minor cleanup

--- main.js
+++ main.js
@@ -32,33 +32,35 @@ document.addEventListener(
         )
 
         //validate contact-me form data
-        const contactMeForm = document.querySelector('form#contact-me-form')
-        contactMeForm.addEventListener(
-            'submit', 
-            (ev)=>{
-                ev.preventDefault()
-                
-                const userData = {
-                    'First Name': document.querySelector('#first-name').value,
-                    'Last Name': document.querySelector('#last-name').value,
-                    'Full Name': '',
-                    'Telephone': document.querySelector('#telephone').value,
-                    'Email': document.querySelector('#email').value,
-                    'Message': document.querySelector('#message').value
+        const currentPageUri = document.location.href
+        if(currentPageUri.includes('#contact-me')){
+            const contactMeForm = document.querySelector('form#contact-me-form')
+            contactMeForm.addEventListener(
+                'submit', 
+                (ev)=>{
+                    ev.preventDefault()
+                    //collect user data
+                    const userData = {
+                        'First Name': document.querySelector('#first-name').value,
+                        'Last Name': document.querySelector('#last-name').value,
+                        'Full Name': '',
+                        'Telephone': document.querySelector('#telephone').value,
+                        'Email': document.querySelector('#email').value,
+                        'Message': document.querySelector('#message').value
+                    }
+                    userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
+                    //validate data
+                    if(
+                        isUserEmailAddressValid(userData['Email']) 
+                                        &&
+                        isUserTelephoneValid(userData['Telephone']) 
+                    ){
+                        alert(`Thank you for leaving a message! I'll be in touch very soon.`)
+                    }else{
+                        alert('Your message has not been sent due to an invalid email and/or telephone number.')
+                    }
                 }
-                userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
-
-                if(
-                    isUserEmailAddressValid(userData['Email']) 
-                                    &&
-                    isUserTelephoneValid(userData['Telephone']) 
-                ){
-                    console.table(userData)
-                    alert(`Thank you for leaving a message! I'll be in touch very soon.`)
-                }else{
-                    alert('Your message has not been sent due to an invalid email and/or telephone number.')
-                }
-            }
-        )
+            )
+        }
     }
 )
