0ecd5b06d955e30057fe5c5fb690537e1c4497cf
ftanekha <farai.tanekha@gmail.com>
2024-07-06 20:07:38 +0100

    Contact form - remove the unnecessary if statement

--- main.js
+++ main.js
@@ -20,35 +20,32 @@ document.addEventListener(
         )
 
         //validate contact-me form data
-        const currentPageUri = document.location.href
-        if(currentPageUri.includes('#contact-me')){
-            const contactMeForm = document.querySelector('form#contact-me-form')
-            contactMeForm.addEventListener(
-                'submit', 
-                (ev)=>{
-                    ev.preventDefault()
-                    //collect user data
-                    const userData = {
-                        'First Name': document.querySelector('#first-name').value,
-                        'Last Name': document.querySelector('#last-name').value,
-                        'Full Name': '',
-                        'Telephone': document.querySelector('#telephone').value,
-                        'Email': document.querySelector('#email').value,
-                        'Message': document.querySelector('#message').value
-                    }
-                    userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
-                    //validate data
-                    if(
-                        isUserEmailAddressValid(userData['Email']) 
-                                        &&
-                        isUserTelephoneValid(userData['Telephone']) 
-                    ){
-                        alert(`Thank you for leaving a message! I'll be in touch very soon.`)
-                    }else{
-                        alert('Your message has not been sent due to an invalid email and/or telephone number.')
-                    }
+        const contactMeForm = document.querySelector('form#contact-me-form')
+        contactMeForm.addEventListener(
+            'submit', 
+            (ev)=>{
+                ev.preventDefault()
+                //collect user data
+                const userData = {
+                    'First Name': document.querySelector('#first-name').value,
+                    'Last Name': document.querySelector('#last-name').value,
+                    'Full Name': '',
+                    'Telephone': document.querySelector('#telephone').value,
+                    'Email': document.querySelector('#email').value,
+                    'Message': document.querySelector('#message').value
                 }
-            )
-        }
+                userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
+                //validate data
+                if(
+                    isUserEmailAddressValid(userData['Email']) 
+                                    &&
+                    isUserTelephoneValid(userData['Telephone']) 
+                ){
+                    alert(`Thank you ${userData['Full Name']} for leaving a message!\nI'll be in touch very soon.😄`)
+                }else{
+                    alert('Your message has not been sent due to an invalid email and/or telephone number.')
+                }
+            }
+        )
     }
 )
