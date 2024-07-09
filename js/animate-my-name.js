51dcbc57b60e2b271f0be22b57e07a09b2bd9e22
ftanekha <farai.tanekha@gmail.com>
2024-07-09 21:32:23 +0100

    Section#Banner - add background image - add fallback background image
    -add filter property to banner ans side-menu
    
    JS
    - use localStorage to animate Banner text only on first page-load

--- js/animate-my-name.js
+++ js/animate-my-name.js
@@ -1,8 +1,13 @@
 $(
     ()=>{
-        const $myName = $('b#my-name')
-        const $myTitle = $('strong#my-title')
-        $myName.fadeOut(500).fadeIn(2000)
-        $myTitle.fadeOut(1000).fadeIn(3000)
+        const hasBannerTextBeenAnimated = localStorage.getItem('hasBannerTextBeenAnimated')
+        if(!hasBannerTextBeenAnimated){
+            const $myName = $('b#my-name')
+            const $myTitle = $('strong#my-title')
+            $myName.fadeOut(1000).fadeIn(2000)
+            $myTitle.fadeOut(2000).fadeIn(2000)
+
+            localStorage.setItem('hasBannerTextBeenAnimated', true)
+        }
     }
 )
