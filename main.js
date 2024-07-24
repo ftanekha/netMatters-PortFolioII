2c39344c76e42f410799179298845e0ac279d68e
ftanekha <farai.tanekha@gmail.com>
2024-07-24 18:27:14 +0100

    remove console error for undefine form reference - use conditional statement to check if user is on correct page to use form

--- main.js
+++ main.js
@@ -20,47 +20,49 @@ document.addEventListener(
         )
 
         //validate contact-me form data
-        const contactMeForm = document.querySelector('form#contact-me-form')
-        contactMeForm.addEventListener(
-            'submit', 
-            (ev)=>{
-                ev.preventDefault()
-                //collect user data
-                const userData = {
-                    'First Name': document.querySelector('#first-name').value,
-                    'Last Name': document.querySelector('#last-name').value,
-                    'Full Name': '',
-                    'Telephone': document.querySelector('#telephone').value,
-                    'Email': document.querySelector('#email').value,
-                    'Message': document.querySelector('#message').value
-                }
-                userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
-                
-                //validate user data
-                if(!isUserTelephoneValid(userData['Telephone'])){
-                    if(!isUserEmailAddressValid(userData['Email'])){
-                        return displayInfoMessage('invalid telephone number and email address')
+        if(document.URL.includes('contact-me') || document.URL.includes('index.html')){
+            const contactMeForm = document.querySelector('form#contact-me-form')
+            contactMeForm.addEventListener(
+                'submit', 
+                (ev)=>{
+                    ev.preventDefault()
+                    //collect user data
+                    const userData = {
+                        'First Name': document.querySelector('#first-name').value,
+                        'Last Name': document.querySelector('#last-name').value,
+                        'Full Name': '',
+                        'Telephone': document.querySelector('#telephone').value,
+                        'Email': document.querySelector('#email').value,
+                        'Message': document.querySelector('#message').value
                     }
-                    if(isUserEmailAddressValid(userData['Email'])){
-                        return displayInfoMessage('invalid telephone number')
+                    userData['Full Name'] = userData['First Name'] + ' ' + userData['Last Name']
+                    
+                    //validate user data
+                    if(!isUserTelephoneValid(userData['Telephone'])){
+                        if(!isUserEmailAddressValid(userData['Email'])){
+                            return displayInfoMessage('invalid telephone number and email address')
+                        }
+                        if(isUserEmailAddressValid(userData['Email'])){
+                            return displayInfoMessage('invalid telephone number')
+                        }
+                    }
+                    
+                    if(isUserTelephoneValid(userData['Telephone'])){
+                        if(!isUserEmailAddressValid(userData['Email'])){
+                            return displayInfoMessage('invalid email address')
+                        }
+                    }
+                    
+                    if(
+                        isUserTelephoneValid(userData['Telephone']) 
+                                            &&
+                        isUserEmailAddressValid(userData['Email']) 
+                    ){
+                        displayInfoMessage(`Thank you ${userData['Full Name']} for leaving a message!\nI'll be in touch very soon.😎`)
+                        return contactMeForm.reset()
                     }
                 }
-                
-                if(isUserTelephoneValid(userData['Telephone'])){
-                    if(!isUserEmailAddressValid(userData['Email'])){
-                        return displayInfoMessage('invalid email address')
-                    }
-                }
-                
-                if(
-                    isUserTelephoneValid(userData['Telephone']) 
-                                        &&
-                    isUserEmailAddressValid(userData['Email']) 
-                ){
-                    displayInfoMessage(`Thank you ${userData['Full Name']} for leaving a message!\nI'll be in touch very soon.😎`)
-                    return contactMeForm.reset()
-                }
-            }
-        )
+            )
+        }
     }
 )
