93e2f4bd299507723c807cfbd0a5fbd08619284d
ftanekha <farai.tanekha@gmail.com>
2024-09-15 13:15:36 +0100

    Section#Code-examples ISSUE: after using the goBack button, toggling example images doesn't work# Please enter the commit message for your changes. Lines starting [FIXED] SOL: refactor the js code so that click event aren't all nested inside the top level 'dblclick' event' as that was causing duplication of events

--- js/code-examples.js
+++ js/code-examples.js
@@ -10,148 +10,158 @@ document.addEventListener(
         const examplesListContainers = document.querySelectorAll('.examples-list-container')
         const examples = document.querySelectorAll('.example')
         const accordions = document.querySelectorAll('.accordion')
-        const accordionControls = document.querySelectorAll('.accordion-control');
+        const accordionControls = document.querySelectorAll('.accordion-control')
+
+        let projectOnDisplay = ''
+        let currentExampleClassName = '';
 
         [topFirstChild, topSecondChild, bottomChild].forEach(
             el => {
-                ///////////// OPEN EXAMPLES MODAL /////////////////////////////////////////////////
                 el.addEventListener(
+                    /////////////OPEN EXAMPLES MODAL///
                     'dblclick', displayModal = (ev)=> {
                         ev.stopPropagation()
                         //
-                        const projectOnDisplay = ev.target
-                        projectOnDisplay .removeEventListener('dblclick', displayModal)
+                        projectOnDisplay = ev.target
+                        projectOnDisplay.removeEventListener('dblclick', displayModal)
                         //
                         projectOnDisplay.classList.add('front')
                         projectOnDisplay .title = ''
-                        //////show examples///////////////
-                        function displayListOfExamples(){
-                            examplesListContainers.forEach(
-                                examplesListContainer=> {
-                                    //examplesListContainer.style.display = 'none !important'
-                                    examplesListContainer.style.position = 'static'
-                                    examplesListContainer.style.bottom = ''
-                                    // examplesListContainer.removeEventListener('dblclick', displayModal)
-                                }
-                            )
-                        }
-                        
-                        examplesLink.forEach(
-                            link => {
-                                link.addEventListener(
-                                    'click', ({target})=>{
-                                        examples.forEach(
-                                            example => example.style.display = 'none'
-                                        )
-                                        //hide list of examples
-                                        examplesListContainers.forEach(
-                                            examplesListContainer=> {
-                                                //examplesListContainer.style.display = 'none !important'
-                                                examplesListContainer.style.position = 'fixed'
-                                                examplesListContainer.style.bottom = '-1000px'
-                                            }
-                                        )
-                                        //only display corresponding example
-                                        const exampleClassNames = [
-                                            'choosing-random-color', 'flexbox', 'styling-images-in-collection', 
-                                            'import-export', 'fetch-api', 'local-storage'
-                                        ]
-                                        
-                                        let currentExampleClassName = ''
-                                        exampleClassNames.forEach(
-                                            exampleClassName => {
-                                                if(target.classList.contains(exampleClassName)) {
-                                                    document.querySelector(`#${exampleClassName}-example-container`).style.display = 'block'
-                                                    currentExampleClassName = exampleClassName
-
-                                                     //toggle images
-                                                    accordionControls.forEach(
-                                                        accordionControl => accordionControl.addEventListener(
-                                                            'click', ({target})=>{
-                                                                const accordion = target.parentElement.nextElementSibling
-
-                                                                const acd = accordion.style.display
-                                                                if(acd === '') accordion.style.display = 'flex'
-                                                                if(acd === 'none') accordion.style.display = 'flex'
-                                                                if(acd === 'flex') accordion.style.display = 'none'
-                                                            }
-                                                        )
-                                                    )
-                                                }
-                                            }
-                                        )
-                                        //display goBack button
-                                        goBackButtons.forEach(
-                                            button => {
-                                                if(button.parentElement === projectOnDisplay){
-                                                    button.style.display = 'flex'
-                                                    button.title = 'go back'
-                                                    button.addEventListener(
-                                                        'click', ({target: btn})=>{
-                                                            accordions.forEach(
-                                                                accordion => accordion.style.display = 'none'
-                                                            )
-                                                            //remove example from display
-                                                            document.querySelector(`#${currentExampleClassName}-example-container`).style.display = 'none'
-                                                            //hide goBack button
-                                                            btn.title = ''
-                                                            btn.style.display = 'none'
-                                                            //bring back example list container
-                                                            displayListOfExamples()
-                                                        }
-                                                    )
-                                                }
-                                            }
-                                        )
-                                    }
-                                )
-                            }
-                        )
-                        ///////////CLOSE/COLLAPSE EXAMPLES MODAL /////////////////////////
+                        ///////////CLOSE/COLLAPSE EXAMPLES MODAL///
                         closeButtons.forEach(
                             btn => {
-
                                 if(btn.parentElement === projectOnDisplay){
                                     btn.style.display = 'inline-flex'
                                     btn.title =  'close Examples modal'
-                                    btn.addEventListener(
-                                        'click', ()=>{
-                                            accordions.forEach(
-                                                accordion => accordion.style.display = 'none'
-                                            )
-
-                                            goBackButtons.forEach(
-                                                button => {
-                                                    button.style.display = 'none'
-                                                    button.title = ''
-                                                }
-                                            )
-
-                                            examples.forEach(
-                                                example => example.style.display = 'none'
-                                            )
-
-                                            displayListOfExamples()
-
-                                            children.forEach(
-                                                child => {
-                                                    child.style.display = 'inline-flex'
-                                                }
-                                            )
-                                
-                                            btn.style.display = 'none'
-
-                                            projectOnDisplay.addEventListener('dblclick', displayModal)
-                                            projectOnDisplay.title = 'Double-Click to expand.'
-                                            projectOnDisplay.classList.remove('front')
-                                        }
-                                    )
                                 }
                             }
                         )
                     }
                 )
             }
+        )
+        //Code examples
+        examplesLink.forEach(
+            link => {
+                link.addEventListener(
+                    'click', ({target})=>{
+                        examples.forEach(
+                            example => example.style.display = 'none'
+                        )
+                        //hide list of examples
+                        examplesListContainers.forEach(
+                            examplesListContainer=> {
+                                //examplesListContainer.style.display = 'none !important'
+                                examplesListContainer.style.position = 'fixed'
+                                examplesListContainer.style.bottom = '-1000px'
+                            }
+                        )
+                        //only display corresponding example
+                        const exampleClassNames = [
+                            'choosing-random-color', 'flexbox', 'styling-images-in-collection', 
+                            'import-export', 'fetch-api', 'local-storage'
+                        ]
+                        
+                        exampleClassNames.forEach(
+                            exampleClassName => {
+                                if(target.classList.contains(exampleClassName)) {
+                                    document.querySelector(`#${exampleClassName}-example-container`).style.display = 'block'
+                                    currentExampleClassName = exampleClassName
+                                }
+                            }
+                        )
+                        //display goBack button
+                        goBackButtons.forEach(
+                            button => {
+                                if(button.parentElement === projectOnDisplay){
+                                    button.style.display = 'flex'
+                                    button.title = 'go back'
+                                }
+                            }
+                        )
+                    }
+                )
+            }
+        )
+        //Display list of examples
+        function displayListOfExamples(){
+            examplesListContainers.forEach(
+                examplesListContainer=> {
+                    //examplesListContainer.style.display = 'none !important'
+                    examplesListContainer.style.position = 'static'
+                    examplesListContainer.style.bottom = ''
+                    // examplesListContainer.removeEventListener('dblclick', displayModal)
+                }
+            )
+        }
+        
+        goBackButtons.forEach(
+            button => {
+                button.addEventListener(
+                    'click', ({target: btn})=>{
+                        accordions.forEach(
+                            accordion => accordion.style.display = 'none'
+                        )
+                        //remove example from display
+                        document.querySelector(`#${currentExampleClassName}-example-container`).style.display = 'none'
+                        //hide goBack button
+                        btn.title = ''
+                        btn.style.display = 'none'
+                        //bring back example list container
+                        displayListOfExamples()
+                    }
+                )
+            }
+        )
+        
+        closeButtons.forEach(
+            btn => {
+                btn.addEventListener(
+                    'click', ()=>{
+                        accordions.forEach(
+                            accordion => accordion.style.display = 'none'
+                        )
+        
+                        goBackButtons.forEach(
+                            button => {
+                                button.style.display = 'none'
+                                button.title = ''
+                            }
+                        )
+        
+                        examples.forEach(
+                            example => example.style.display = 'none'
+                        )
+        
+                        displayListOfExamples()
+        
+                        children.forEach(
+                            child => {
+                                child.style.display = 'inline-flex'
+                            }
+                        )
+            
+                        btn.style.display = 'none'
+        
+                        projectOnDisplay.addEventListener('dblclick', displayModal)
+                        projectOnDisplay.title = 'Double-Click to expand.'
+                        projectOnDisplay.classList.remove('front')
+                    }
+                )
+            }
+        )
+        //Toggle example images
+        accordionControls.forEach(
+            accordionControl => accordionControl.addEventListener(
+                'click', ({target})=>{
+                    const accordion = target.parentElement.nextElementSibling
+
+                    const acd = accordion.style.display
+                    if(acd === '') accordion.style.display = 'flex'
+                    if(acd === 'none') accordion.style.display = 'flex'
+                    if(acd === 'flex') accordion.style.display = 'none'
+                }
+            )
         )
     }
 )
