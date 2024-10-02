a48fba29c579f9fc7ab3ce6831852a79e9b5d34e
ftanekha <farai.tanekha@gmail.com>
2024-10-02 20:32:58 +0100

    testin -- no-edit

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ document.addEventListener(
                         return displayInfoMessage(errors)
                     }else{
                         // POST data & display success message
-                        const url = './post-form-data.php'
+                        const url = 'http://dclhofrauw.us14.qoddiapp.com/post-form-data.php'
                         console.log('URL:', url)
                         fetch(
                             url, 
