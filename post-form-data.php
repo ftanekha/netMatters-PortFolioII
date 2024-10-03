37e5af2e42de51d379284b51607322a0f69e00ad
ftanekha <farai.tanekha@gmail.com>
2024-10-03 14:44:54 +0100

    testing qoddi db connection --incluing all error reporting -- no-edit

--- post-form-data.php
+++ post-form-data.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
 
 include "loadenv.php";
 #set up PHPMailer
