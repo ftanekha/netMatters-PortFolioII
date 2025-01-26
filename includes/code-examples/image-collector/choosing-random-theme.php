1f6ce383a72190d5c311e9e68a266622feb3dd14
ftanekha <farai.tanekha@gmail.com>
2025-01-26 12:46:41 +0000

    Section#Code-Examples >> choosing random theme -apply responsive design to images

--- includes/code-examples/image-collector/choosing-random-theme.php
+++ includes/code-examples/image-collector/choosing-random-theme.php
@@ -3,7 +3,7 @@
         <a href="javascript:;" class="accordion-control">Click here to toggle image</a>
     </p>
     <div class="overal-page-color-theme accordion">
-        <img src="../assets/images/code-examples/image-collector/choosing-random-color-theme.jpeg" class="chestnut"
+        <img src="../assets/images/code-examples/image-collector/choosing-random-color-theme.jpeg" class="graphic"
             alt="overall page color theme" title="the overall page color theme"
         >
     </div>
@@ -11,10 +11,10 @@
         <a href="javascript:;" class="accordion-control">Click here to toggle images</a>
     </p>
     <div class="choosing-the-color-theme-with-code accordion">
-        <img src="../assets/images/code-examples/image-collector/custom-error-message.jpeg" class="chestnut"
+        <img src="../assets/images/code-examples/image-collector/custom-error-message.jpeg" class="graphic"
             alt="custom error message example" title="custom error message example"
         >
-        <img src="../assets/images/code-examples/image-collector/custom-error-message-vs-js-alert.png" class="chestnut"
+        <img src="../assets/images/code-examples/image-collector/custom-error-message-vs-js-alert.png" class="graphic"
             alt="custom error message vs JavaScript alert" title="custom error message vs JavaScript alert"
         >
     </div>
@@ -23,10 +23,10 @@
     </p>
     <div class="choosing-the-color-theme-with-code accordion">
         <img src="../assets/images/code-examples/image-collector/choosing-random-color-theme-using-bulma-danger.png" 
-            alt="using Bulma's danger color" title="using Bulma's danger color" class="small"
+            alt="using Bulma's danger color" title="using Bulma's danger color" class="medium"
         >
         <img src="../assets/images/code-examples/image-collector/choosing-random-color-theme-using-scss-lighten-function.png" 
-            alt="using the SCSS lighten function" title="SCSS lighten function example" class="small"
+            alt="using the SCSS lighten function" title="SCSS lighten function example" class="medium"
         >
     </div>
 </div>
